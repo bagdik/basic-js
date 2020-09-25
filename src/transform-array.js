873f7022af16c9f6a89b6da7dde86ab013c7c4d1
Bahdan Pryimachuk <bahdanchik@tut.by>
2020-09-26 02:02:00 +0300

    feat: Implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let result = null;
+  try{
+    if(!Array.isArray(arr)){
+      throw new Error('The argument is not an Array'); 
+    } else if (arr.length === 0) {
+      result = [];
+    } else {
+      result = arr.slice();
+    }
+    arr.forEach((element, index) => {
+      if(element === '--discard-next'){
+        if(index === arr.length - 1){
+          result.splice(index, 1);
+        } else {
+          result.splice(index, 2);
+        }
+      } else if (element === '--discard-prev'){
+        if (index === 0){
+          result.splice(0, 1);
+        } else {
+          if (arr.length === result.length){
+            result.splice(index - 1, 2);
+          } else {
+            result.splice(index - 2, 1);
+          }
+        }
+      } else if(element === '--double-next'){
+        if(index === arr.length - 1){
+          result.splice(index, 1);
+        } else {
+          result.splice(index, 1, arr[index + 1]);
+        }
+      } else if (element === '--double-prev') {
+        if (index === 0){
+          result.splice(0, 1);
+        } else if(arr.length === result.length){
+          result.splice(index, 1, arr[index - 1]);
+        } else {
+          result.splice(index - 2, 1);
+        }
+      } 
+    });
+  } catch(err) {
+    if (err.name === 'Error') {
+      result = 'THROWN';
+      throw err;
+    }
+  }
+return result;
 };
