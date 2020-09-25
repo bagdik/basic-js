e50123855198b324caf9c1346b8746940b9832b6
Bahdan Pryimachuk <bahdanchik@tut.by>
2020-09-25 09:26:04 +0300

    feat: Implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = [];
+  if (!Array.isArray(members)) return false;
+  members.forEach(member => {
+    if (typeof(member) === 'string') {
+      teamName.push(member.trim()[0].toUpperCase());
+    }
+  });
+  return teamName.sort((a, b) => {
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+  }).join('');
 };
